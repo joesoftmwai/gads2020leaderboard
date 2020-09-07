b0a744b5c0142f1b6fd560b930f82dc5c60afb0f
Mwai Joseph <joesoftmwai@gmail.com>
2020-09-07 14:58:21 +0300

    Started working on the app, implemented the landing activity

---
+++ app/src/main/java/com/joesoft/gads2020leaderboard/LandingActivity.java
@@ -0,0 +1,14 @@
+package com.joesoft.gads2020leaderboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
