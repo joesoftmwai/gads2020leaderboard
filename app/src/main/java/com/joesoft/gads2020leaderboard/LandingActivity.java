fedf7ecb5306f45754f12f2727f6315234b4de63
Mwai Joseph <joesoftmwai@gmail.com>
2020-09-08 16:46:00 +0300

    iplemented project submission service

--- app/src/main/java/com/joesoft/gads2020leaderboard/LandingActivity.java
+++ app/src/main/java/com/joesoft/gads2020leaderboard/LandingActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
 
     }
 
--- app/src/main/java/com/joesoft/gads2020leaderboard/ProjectSubmissionActivity.java
+++ app/src/main/java/com/joesoft/gads2020leaderboard/ProjectSubmissionActivity.java
@@ -1,11 +1,13 @@
 package com.joesoft.gads2020leaderboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,16 +15,24 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.Group;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.joesoft.gads2020leaderboard.services.ProjectSubmissionService;
+import com.joesoft.gads2020leaderboard.services.ServiceBuilder;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProjectSubmissionActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = ProjectSubmissionActivity.class.getSimpleName();
     private ImageView mBackArrow;
     private TextInputEditText mTextInputFirstName;
     private TextInputEditText mTextInputLastName;
     private TextInputEditText mTextInputEmail;
     private TextInputEditText mTextInputProjectLink;
     private Button mButtonSubmit;
+    private Group mGroupContent;
+    private ProgressBar mProgressBar;
 
     private SweetAlertDialog mSweetAlertDialog;
 
@@ -36,6 +46,8 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
         mTextInputEmail = findViewById(R.id.text_input_email);
         mTextInputProjectLink = findViewById(R.id.text_input_project_link);
         mButtonSubmit = findViewById(R.id.btn_submit);
+        mGroupContent = findViewById(R.id.group_content);
+        mProgressBar = findViewById(R.id.progressBar);
 
         mBackArrow.setOnClickListener(this);
         mButtonSubmit.setOnClickListener(this);
@@ -61,6 +73,7 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
                 && !mTextInputProjectLink.getText().toString().isEmpty()) {
             // check if email is valid
             if (validateEmail(mTextInputEmail)) {
+                mGroupContent.setVisibility(View.INVISIBLE);
                 mSweetAlertDialog = new SweetAlertDialog(
                         ProjectSubmissionActivity.this, SweetAlertDialog.WARNING_TYPE);
                 mSweetAlertDialog
@@ -84,7 +97,39 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
     }
 
     private void submitProject() {
+        showProgressBar();
 
+        ProjectSubmissionService projectSubmissionService
+                = ServiceBuilder.buildService(ProjectSubmissionService.class);
+        Call<Void> submitProjectRequest
+                = projectSubmissionService.newProject(
+                mTextInputFirstName.getText().toString(),
+                mTextInputLastName.getText().toString(),
+                mTextInputEmail.getText().toString(),
+                mTextInputProjectLink.getText().toString()
+        );
+
+        submitProjectRequest.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "onResponse: submitted: " +response.body());
+                    showSuccessDialog();
+
+                } else {
+                    Log.d(TAG, "onResponse: failed submitting: ");
+                    showFailureDialog();
+                }
+                hideProgressBar();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.d(TAG, "onFailure: failed " + t.getCause());
+                showFailureDialog();
+                hideProgressBar();
+            }
+        });
     }
 
     public boolean validateEmail(TextInputEditText email) {
@@ -96,5 +141,30 @@ public class ProjectSubmissionActivity extends AppCompatActivity implements View
         return false;
     }
 
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+
+    private void showSuccessDialog() {
+        mSweetAlertDialog = new SweetAlertDialog(
+                ProjectSubmissionActivity.this, SweetAlertDialog.SUCCESS_TYPE);
+        mSweetAlertDialog
+                .setContentText("Submission Successful")
+                .show();
+
+    }
+
+    private void showFailureDialog() {
+        mSweetAlertDialog = new SweetAlertDialog(
+                ProjectSubmissionActivity.this, SweetAlertDialog.ERROR_TYPE);
+        mSweetAlertDialog
+                .setContentText("Submission not Successful")
+                .show();
+    }
 
 }
